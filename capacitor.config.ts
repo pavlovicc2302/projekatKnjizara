b5c3d692b5594566c8b7027ddd92bbb0c314b514
Ana Pavlovic <pavlovicana256@gmail.com>
2024-03-13 13:01:34 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'knjizara',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
