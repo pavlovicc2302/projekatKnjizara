0d19f193c5263febf00954dc20daceab295154f6
SaraRis <sr20200340@student.fon.bg.ac.rs>
2024-05-15 00:02:12 +0200

    Dodata logika za logout

--- src/app/home/profil/profil.page.ts
+++ src/app/home/profil/profil.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-profil',
@@ -8,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
 export class ProfilPage implements OnInit {
   ulogovani: any;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {
+  }
 
   ngOnInit() {
     this.ulogovani = localStorage.getItem("ulogovani")
+  }
+
+  logOut() {
+    this.authService.logOut();
+    localStorage.removeItem('ulogovani');
+    this.router.navigateByUrl('/login');
   }
 
 }
